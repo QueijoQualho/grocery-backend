fc9c2f7220a8edd5231f250ea4dabd2626082e47
Pedro Jesus <pedromra.04@gmail.com>
2024-04-29 20:19:33 -0300

    add postgres render server

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -9,12 +9,9 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.configService.get<string>('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_USERNAME'),
-      password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_NAME'),
+      url: this.configService.get<string>('DATABASE_URL'),
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      ssl: true,
     };
   }
 }
--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -3,13 +3,10 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
+  ssl: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
